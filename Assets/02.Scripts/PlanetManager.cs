4db73e7cd02108573c7df03e1b05a3f0b2c2a507
Gibom <rlqha123@gmail.com>
2016-06-29 17:32:11 +0900

    Game Scene 카메라 세팅 및 Object Scale 수정

---
+++ Assets/02.Scripts/PlanetManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetManager : MonoBehaviour
+{
+    public GameObject planet;
+    public float spawnTime = 2.0f;
+
+    float deltaSpawnTime = 0.0f;
+
+    int spawnCnt = 1;
+    int maxSpawnCnt = 10;
+
+    GameObject[] planetPool;
+    int poolSize = 10;
+
+    void Start()
+    {
+        planetPool = new GameObject[poolSize];
+        for(int i=0; i< poolSize; ++i)
+        {
+            planetPool[i] = Instantiate(planet) as GameObject;
+            planetPool[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (spawnCnt > maxSpawnCnt)
+            return;
+
+        deltaSpawnTime += Time.deltaTime;
+
+        if (deltaSpawnTime > spawnTime)
+        {
+            deltaSpawnTime = 0.0f;
+            for(int i = 0; i < poolSize; i++)
+            {
+                if (planetPool[i].activeSelf == true)
+                    continue;
+
+                int x = Random.Range(-20, 20);
+                planetPool[i].transform.position = new Vector3(x, 0.1f, 20.0f);
+                planetPool[i].SetActive(true);
+
+                planetPool[i].name = "Planet_" + spawnCnt;
+                ++spawnCnt;
+                break;
+            }
+            
+        }
+    }
+}
