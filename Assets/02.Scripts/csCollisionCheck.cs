391b182ddd6190c3176d64a02a58c41291b7148a
Gibom <rlqha123@gmail.com>
2016-06-30 17:22:56 +0900

    Add Function (CollisionCheck) [Script]  - 02.Scripts\csCollisionCheck    Planet -> Rigidbody , Player -> Box Collider (is Trigger)

---
+++ Assets/02.Scripts/csCollisionCheck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csCollisionCheck : MonoBehaviour {
+
+    void  OnCollisionEnter (Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+}
