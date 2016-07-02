dc20acb8d7bdebee8b798f234514c397c27e54c1
Gibom <rlqha123@gmail.com>
2016-07-02 21:46:07 +0900

    Changes [Scene]   - 씬 이름 변경 Sub- > WorldMap [Prefabs]   - Star 삭제   - Stars 폴더 생성 및 별 크기별 분류 Prefab 생성(VerySmall, Small, Medium, Big)   - Prefab에 포함된 Light 효과를 위한 flare 추가 (Blue Light Glow0-3)
    Add Function (DistanceCalc)
    [Script]
     - 02.Scripts\DistanceCalc
       거리계산기(기준점, 목표지점 Object를 각각 DistanceCalculator(Object)에 매치시키킨후 실행시 Debug 출력)

--- Assets/02.Scripts/ButtonController.cs
+++ Assets/02.Scripts/ButtonController.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class ButtonController : MonoBehaviour {
 
+    public void explore()
+    {
+        Debug.Log("행성을 탐사합니다!");
+    }
+
     public void pass()
     {
         Time.timeScale = 1;
         GameManager.Instance().exploreUi.SetActive(false);
     }
+
     
 }
