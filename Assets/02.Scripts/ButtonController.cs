b7ad7c8f5ab3dadb62f051b4b95e30701e73abc7
leesq <leesq87@gamil.com>
2016-07-05 17:29:57 +0900

    3D LowPoly asset 추가 mainScene 일부 버그 수정 (RayCast버그 남아있음) manageScene 추가 UI구현

--- Assets/02.Scripts/ButtonController.cs
+++ Assets/02.Scripts/ButtonController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour {
 
@@ -14,5 +16,62 @@ public class ButtonController : MonoBehaviour {
         GameManager.Instance().exploreUi.SetActive(false);
     }
 
-    
+
+    public void TransSceneToShop()
+    {
+        //SceneManager.LoadScene("asdf");
+        Debug.Log("scene Trans to shop");
+    }
+
+
+    public void TransSceneToBook()
+    {
+        //SceneManager.LoadScene("Book");
+        Debug.Log("scne trans to Book");
+    }
+
+    public void TransSceneToMap()
+    {
+        //SceneManager.LoadScene("Map");
+        Debug.Log("scene trans to map");
+    }
+
+    public void VisibleSetting()
+    {
+        GameObject.Find("Canvas").transform.FindChild("SettingPanal").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.FindChild("DragZone").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.FindChild("BlockPanal").gameObject.SetActive(true);
+        GameObject planet = GameObject.Find("death_planet");
+        planet.gameObject.layer = 2;
+    }
+
+
+    public void Confirm()
+    {
+        Debug.Log("confirm");
+        GameObject.Find("Canvas/SettingPanal").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.FindChild("DragZone").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.FindChild("BlockPanal").gameObject.SetActive(false);
+        GameObject planet = GameObject.Find("death_planet");
+        planet.gameObject.layer = 0;
+
+    }
+
+    public void Cancel()
+    {
+        Debug.Log("Cancel");
+        GameObject.Find("Canvas/SettingPanal").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.FindChild("DragZone").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.FindChild("BlockPanal").gameObject.SetActive(false);
+        GameObject planet = GameObject.Find("death_planet");
+        planet.gameObject.layer = 0;
+
+    }
+
+    public void TransSceneToWorldMap()
+    {
+        //SceneManager.LoadScene("WorldMap");
+        Debug.Log("scene Trans to WorldMap");
+    }
+
 }
