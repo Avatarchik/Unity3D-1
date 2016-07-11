aa99063ab3e7e1c958f257d5451615efab2ad038
leesq87 <leesq87@gmail.com>
2016-07-12 02:44:21 +0900

    Main Scene UI적용 anoterPos Scene 삭제 Star Scene UI적용 Main,Star,ManagePlanet skybox적용
    MainScene에 DragRotate 수정중

--- Assets/02.Scripts/MainScene/csDragRotation.cs
+++ Assets/02.Scripts/MainScene/csDragRotation.cs
@@ -9,11 +9,22 @@ public class csDragRotation : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
 
     Vector2 oldPos;
     Vector2 newPos;
+    Vector2 delPos;
 
     GameObject obj;
+    GameObject RotateBase;
+    GameObject planet;
+
+    Vector3 planetRotation = new Vector3(0, 0, 0);
+
+
     void Start()
     {
+        //planetRotation = new Vector3(0, 0, 0);
         obj = GameObject.Find("Main Camera");
+
+        RotateBase = GameObject.Find("PlanetPosition");
+        planet = GameObject.Find("PlanetPosition/death_planet");
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -24,14 +35,19 @@ public class csDragRotation : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     }
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
         newPos = new Vector2(eventData.position.x, eventData.position.y);
-        Debug.Log(eventData.delta);
 
-        GameObject obj = GameObject.Find("death_planet/death_planet");
-        //obj.transform.Rotate(new Vector3(eventData.delta.y/dragRate, 0, -eventData.delta.x/ dragRate));
-        obj.transform.Rotate(new Vector3((newPos.y - oldPos.y) / dragRate, 0, -(newPos.x - oldPos.x) / dragRate));
+        delPos = new Vector2(eventData.delta.x, eventData.delta.y);
+        Debug.Log(delPos);
+        RotateBase.transform.Rotate(new Vector3(eventData.delta.y, -eventData.delta.x,0 ));
 
+        //GameObject obj = GameObject.Find("PlanetPosition/death_planet");
+        //obj.transform.Rotate(new Vector3(eventData.delta.y , 0, -eventData.delta.x ));
+        //Quaternion.w
+        ////obj.transform.Rotate(new Vector3((newPos.y - oldPos.y) / dragRate, 0, -(newPos.x - oldPos.x) / dragRate));
+        //obj.transform.Rotate(RotateBase.transform.localEulerAngles/dragRate);
+        //obj.transform.localRotation = Quaternion.Euler(new Vector3(RotateBase.transform.eulerAngles.x/dragRate, 0, -RotateBase.transform.eulerAngles.z/dragRate));
 
     }
 
@@ -39,18 +55,26 @@ public class csDragRotation : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     {
         Debug.Log("OnDragEnd");
 
+        //planet.gameObject.transform.rotation = RotateBase.gameObject.transform.rotation;
+        calculateRotation();
+        RotateBase.transform.localRotation = Quaternion.identity;
+        planet.transform.Rotate(planetRotation);
+
+
+
+        //GameObject RotateBase = GameObject.Find("RotateBase");
+        //RotateBase.transform.rotation = Quaternion.identity;
+        //RotateBase.transform.localRotation = Quaternion.identity;
+        //RotateBase.transform.Rotate(new Vector3(0,0,0));
+        //RotateBase.transform.Rotate()
+
         csScreenPointTouch script = obj.GetComponent<csScreenPointTouch>();
 
-        script.dragFalse();
+        script.dragTrue();
     }
-
-    IEnumerator dragFalse()
+    
+    void calculateRotation()
     {
-        Debug.Log("1");
-        yield return new WaitForSeconds(0.5f);
-        Debug.Log("2");
-        csScreenPointTouch script = obj.GetComponent<csScreenPointTouch>();
-        script.dragFalse();
+        planetRotation = RotateBase.transform.rotation.eulerAngles;
     }
-
 }
