dd26ee4eb787289804fe68c98ebb3f187c4aac7a
Gibom <rlqha123@gmail.com>
2016-07-11 23:42:31 +0900

    [Scripts] Explore 폴더 추가 및 관련 Script 이동 WorldMap 폴더 추가(WorldMapManager, wmDragRotation, wmPinch 생성)
    [Resources]
    탐사준비화면 UI 이미지 추가

--- Assets/02.Scripts/WorldMap/wmDragRotation.cs
+++ Assets/02.Scripts/WorldMap/wmDragRotation.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using UnityEngine.EventSystems;
 
 
-public class csDragRotation : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class wmDragRotation : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
 
     public float dragRate = 40;
 
@@ -15,23 +16,26 @@ public class csDragRotation : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     {
         obj = GameObject.Find("Main Camera");
     }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        oldPos = new Vector2(eventData.position.x,eventData.position.y);
+        oldPos = new Vector2(eventData.position.x, eventData.position.y);
         Debug.Log("OnBeginDrag");
         csScreenPointTouch script = obj.GetComponent<csScreenPointTouch>();
         script.dragTrue();
     }
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
-        newPos = new Vector2(eventData.position.x, eventData.position.y);
-        Debug.Log(eventData.delta);
+        if (Input.touchCount == 1)
+        {
+            Debug.Log("OnDrag");
+            newPos = new Vector2(eventData.position.x, eventData.position.y);
+            Debug.Log(eventData.delta);
 
-        GameObject obj = GameObject.Find("PlanetPosition/death_planet");
-        //obj.transform.Rotate(new Vector3(eventData.delta.y/dragRate, 0, -eventData.delta.x/ dragRate));
-        obj.transform.Rotate(new Vector3((newPos.y - oldPos.y) / dragRate, 0, -(newPos.x - oldPos.x) / dragRate));
-
+            GameObject obj = GameObject.Find("Galaxy");
+            //obj.transform.Rotate(new Vector3(eventData.delta.y/dragRate, 0, -eventData.delta.x/ dragRate));
+            obj.transform.Rotate(new Vector3((newPos.y - oldPos.y) / dragRate, 0, -(newPos.x - oldPos.x) / dragRate));
+        }
 
     }
 
