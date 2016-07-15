a4ca220552d633e0b0c21485dd9041fa4e1169c8
leesq <leesq87@gamil.com>
2016-07-15 10:41:54 +0900

    planet 폴더 prefabs 수정요청

--- Assets/02.Scripts/FusionScript.cs
+++ Assets/02.Scripts/FusionScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 
 public class FusionScript : MonoBehaviour {
@@ -16,6 +17,20 @@ public class FusionScript : MonoBehaviour {
     public GameObject SingleTon;
 
 
+    public Text canNum;
+    public Text TextMin;
+    public Text TextMax;
+    public Text TextMakeNum;
+
+    public Image ImgResult;
+    public Image ImgSum1;
+    public Image ImgSum2;
+    public Image ImgSum3;
+
+    //public List<Image> EnergyIcon = new List<Image>();
+    public List<Sprite> EnergyIcon = new List<Sprite>();
+
+
     public void setText()
     {
         TextPENum.GetComponent<Text>().text = SingleTon.GetComponent<MainSingleTon>().cPE + "";
@@ -26,6 +41,33 @@ public class FusionScript : MonoBehaviour {
         TextYENum.GetComponent<Text>().text = SingleTon.GetComponent<MainSingleTon>().cYE + "";
         TextBENum.GetComponent<Text>().text = SingleTon.GetComponent<MainSingleTon>().cBE + "";
     }
+
+    void Start()
+    {
+        setIcon(0, 0, 0);
+    }
+
+
+    public void setIcon(int Result, int Sum1, int Sum3)
+    {
+        ImgResult.GetComponent<Image>().sprite = EnergyIcon[Result];
+        ImgSum1.GetComponent<Image>().sprite = EnergyIcon[Sum1];
+        ImgSum3.GetComponent<Image>().sprite = EnergyIcon[Sum3];
+        ImgSum2.GetComponent<Image>().gameObject.SetActive(false);
+        
+    }
+
+    public void setIcon(int Result, int Sum1, int Sum2, int Sum3)
+    {
+        ImgResult.GetComponent<Image>().sprite = EnergyIcon[Result];
+        ImgSum1.GetComponent<Image>().sprite = EnergyIcon[Sum1];
+        ImgSum2.GetComponent<Image>().sprite = EnergyIcon[Sum2];
+        ImgSum3.GetComponent<Image>().sprite = EnergyIcon[Sum3];
+
+    }
+
+
+
 
 
 
