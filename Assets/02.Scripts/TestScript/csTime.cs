2538738380bc4a4ff256bc26bca66e39f815b388
leesq <leesq87@gamil.com>
2016-07-21 14:54:41 +0900

    prefabs 수정 testScene (sql 및 datetime test 추가됨)

---
+++ Assets/02.Scripts/TestScript/csTime.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csTime : MonoBehaviour {
+
+
+    public string touchTime;
+
+    public System.DateTime temp = System.DateTime.Now;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            System.DateTime a = System.DateTime.Now;
+            touchTime = System.DateTime.Now.ToString();
+            Debug.Log(System.DateTime.Now);
+            Debug.Log(System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            Debug.Log("Temp : " + temp);
+
+            string time2 = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Debug.Log(time2);
+            //Debug.Log(System.Convert.ToDateTime(time2));
+            //System.Convert.ToDateTime(time2);
+            Debug.Log(System.DateTime.ParseExact(time2, "yyyy-MM-dd HH:mm:ss", null));
+            System.DateTime temp2 = System.DateTime.ParseExact(time2, "yyyy-MM-dd HH:mm:ss", null);
+            //System.Convert.ToDateTime
+            System.TimeSpan diff1 = temp2 - temp;
+            //Debug.Log("diff = " + diff1);
+            Debug.Log(diff1);
+
+
+        }
+    }
+
+}
