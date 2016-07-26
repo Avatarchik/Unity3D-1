c20932ae37cd50c3e975fa0b078c5ad442876369
leesq87 <leesq87@gmail.com>
2016-07-27 01:39:20 +0900

    Intro추가

--- Assets/02.Scripts/PlanetList.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[System.Serializable] //<== MonoBehaviour가 아닌 클래스에 대해 Inspector에 나타납니다.  
-public class Testitem
-{
-    //public GameObject planet;
-    //public GameObject[] planet;    // 하위에 넣어질 배열 변수  
-}
---
+++ Assets/02.Scripts/Title/StartScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class StartScript : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        if(Input.touchCount >= 1)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+
+}
