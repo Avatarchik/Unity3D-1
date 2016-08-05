8d535410cb56de3fb8e85bbaf13d86a89d4859a2
leesq <leesq87@gamil.com>
2016-08-05 13:41:03 +0900

    음슴

---
+++ Assets/02.Scripts/StoreDrag.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class StoreDrag : ScrollRect {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
